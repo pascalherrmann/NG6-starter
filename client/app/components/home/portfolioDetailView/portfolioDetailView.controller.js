3a7db81596c639412899a0a339a7b61ee6c27801
Andreas Frohwerk <andreas.frohwerk@gmail.de>
2017-03-24 18:54:52 +0100

    Make max bar width = 100% (more or less)

--- client/app/components/home/portfolioDetailView/portfolioDetailView.controller.js
+++ client/app/components/home/portfolioDetailView/portfolioDetailView.controller.js
@@ -2,8 +2,10 @@ class PortfolioDetailViewController {
   constructor() {
     const round = (n) => Math.round(n * 10) / 10;
     this.votes = [0, 2, 3, 17, 29];
+    this.maxVotes = () => Math.max(...this.votes);
     this.totalVotes = () => this.votes.reduce((acc, val) => acc + val);
     this.totalScore = () => round(this.votes.reduce((acc, val, i) => acc + (i + 1) * val) / this.totalVotes());
+    this.barWidth = (i) => ((this.votes[i-1] / this.maxVotes()) * 100) - 20;
   }
 }
 
