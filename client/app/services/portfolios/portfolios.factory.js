9367263ad5fe4cc31e252df0b03adc9f47f0439b
Pascal Herrmann <pasher1@web.de>
2017-03-25 16:05:30 +0100

    .

--- client/app/services/portfolios/portfolios.factory.js
+++ client/app/services/portfolios/portfolios.factory.js
@@ -1,8 +1,8 @@
 const PortfoliosFactory = ($http) => {
   return {
     list: () => $http.get('/portfolios'), //'http://portfolio-service.mybluemix.net/portfolios'
-    // listByTwitterUsername: (twitterUsername) => $http.get('/portfolios/user/'+twitterUsername), //'http://portfolio-service.mybluemix.net/portfolios'
-    listByTwitterUsername: (twitterUsername) => $http.get('/portfolios'), //'http://portfolio-service.mybluemix.net/portfolios'
+    listByTwitterUsername: (twitterUsername) => $http.get('/portfolios/user/'+twitterUsername), //'http://portfolio-service.mybluemix.net/portfolios'
+   // listByTwitterUsername: (twitterUsername) => $http.get('/portfolios/'), //'http://portfolio-service.mybluemix.net/portfolios'
     get: (id) => $http.get('/portfolios/'+id)
   }
 };
