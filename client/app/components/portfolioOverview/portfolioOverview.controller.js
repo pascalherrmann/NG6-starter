469e5d8dd11efb2438e14d086485051755034e9e
Pascal Herrmann <pasher1@web.de>
2017-03-25 14:22:21 +0100

    fehlermeldung

--- client/app/components/portfolioOverview/portfolioOverview.controller.js
+++ client/app/components/portfolioOverview/portfolioOverview.controller.js
@@ -1,49 +1,60 @@
 class PortfolioOverviewController {
-  constructor(portfolios) {
+    constructor(portfolios) {
 
-    var serviceMock = [
-      {
-        id: 23423,
-        name: "Student",
-        stars: 4,
-        description: "Die perfekte Versicherung für junge Leute zum günstigen Preis.",
-        ratings: 32,
-        products: ["reisekranken"]
+        var serviceMock = [
+            {
+                id: 23423,
+                name: "Student",
+                stars: 4,
+                description: "Die perfekte Versicherung für junge Leute zum günstigen Preis.",
+                ratings: 32,
+                products: ["reisekranken"]
       },
-      {
-        "name": "Student Plus",
-        stars: 3,
-        ratings: 4,
-        description: "Die perfekte Versicherung für junge Leute mit eigener Wohnung.",
-        products: ["leben", "hausrat"]
+            {
+                "name": "Student Plus",
+                stars: 3,
+                ratings: 4,
+                description: "Die perfekte Versicherung für junge Leute mit eigener Wohnung.",
+                products: ["leben", "hausrat"]
       },
-      {
-        "name": "Junge Leute Komplettpaket",
-        stars: 5,
-        description: "Die perfekte Versicherung für junge Leute die alles brauchen.",
-        ratings: 48,
-        products: ["leben", "hausrat", "reisekranken", "test"]
+            {
+                "name": "Junge Leute Komplettpaket",
+                stars: 5,
+                description: "Die perfekte Versicherung für junge Leute die alles brauchen.",
+                ratings: 48,
+                products: ["leben", "hausrat", "reisekranken", "test"]
       }]
 
-    this.test = {
-      name: '',
-      description: ''
-    };
+        this.test = {
+            name: '',
+            description: ''
+        };
 
 
-    this.analyze = function () {
+        this.analyze = function () {
 
-      portfolios.listByTwitterUsername(this.twitterUsername).then(
-        // response => console.log(response.data._embedded.portfolios),
-        response => this.portfolios = response.data._embedded.portfolios,
-        //response => this.portfolios = serviceMock,
-        error => console.log(error)
-      );
+            this.loading = true
 
-    };
+            portfolios.listByTwitterUsername(this.twitterUsername).then(
+                response => {
+                    this.loading = false
+                    this.error = null
+                    this.portfolios = response.data._embedded.portfolios
+                    console.log(response.data._embedded.portfolios)
+
+                },
+                error => {
+                    this.loading = false
+                    this.error = error
+                    console.log("ERROR!")
+                    console.log(error)
+                }
+            );
+
+        };
 
 
-  }
+    }
 }
 
 PortfolioOverviewController.$inject = ['portfolios'];
